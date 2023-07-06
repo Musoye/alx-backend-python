0810387a8f9be84100d32065e159d082240cfeac
Musoye <oyebamijimustapha44@gmail.com>
2023-07-06 20:07:24 +0100

    feat: pyhton variable anootation 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Python - Variable annotation'''
+
+
+def to_str(n: float) -> str:
+    '''Python - variable annotation'''
+    return str(n)
--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
+to_str = __import__('3-to_str').to_str
 
-import math
-
-floor = __import__('2-floor').floor
-
-ans = floor(3.14)
-
-print(ans == math.floor(3.14))
-print(floor.__annotations__)
-print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
+pi_str = to_str(3.14)
+print(pi_str == str(3.14))
+print(to_str.__annotations__)
+print("to_str(3.14) returns {} which is a {}".format(pi_str, type(pi_str)))
