c33744c344cf259b21caa41aa97769a1fa440669
Musoye <oyebamijimustapha44@gmail.com>
2023-07-06 19:44:31 +0100

    Basic annotation: concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+def  concat(str1: str, str2: str) -> str:
+    return (str1 + str2)
--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-add = __import__('0-add').add
+concat = __import__('1-concat').concat
 
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
+str1 = "egg"
+str2 = "shell"
+
+print(concat(str1, str2) == "{}{}".format(str1, str2))
+print(concat.__annotations__)
